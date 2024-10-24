5dcf4779167a37cbde8d9ce4576d3fd8696196dd
Nathan Campos <nathan@innoveworkshop.com>
2024-10-24 12:01:33 +0100

    Added a scaffold and created another activity

--- app/src/main/java/pt/iade/games/gamedevedex/MainActivity.kt
+++ app/src/main/java/pt/iade/games/gamedevedex/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pt.iade.games.gamedevedex.models.Project
+import pt.iade.games.gamedevedex.models.Student
+import pt.iade.games.gamedevedex.ui.components.ProjectCard
 import pt.iade.games.gamedevedex.ui.theme.GamedevedexTheme
+import java.net.URI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +29,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GamedevedexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainView()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainView() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Gamedevedex")
+                },
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            ProjectCard(
+                modifier = Modifier.padding(vertical = 20.dp),
+                project = ProjectExample()
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview(showBackground = true)
+fun MainViewPreview() {
     GamedevedexTheme {
-        Greeting("Android")
+        MainView()
     }
+}
+
+fun ProjectExample(): Project {
+    return Project(
+        title = "Among Us",
+        votes = 2,
+        description = "Super sus.",
+        id = 404,
+        semester = 1,
+        assets = listOf(
+            URI.create("https://cdn.mobygames.com/screenshots/12341377-among-us-windows-calling-an-emergency-meeting.png"),
+            URI.create("https://lutris.net/media/games/screenshots/ss_649e19ff657fa518d4c2b45bed7ffdc4264a4b3a.jpg")
+        ),
+        groupMembers = listOf(
+            Student(
+                id = 123,
+                name = "João Pedro",
+                biography = "Love playing Valorant. Currently thinking of switching courses.",
+                mood = "Lucky",
+                avatar = URI.create("https://media.gettyimages.com/photos/cristiano-ronaldo-of-portugal-poses-during-the-official-fifa-world-picture-id450555852?k=6&m=450555852&s=612x612&w=0&h=aUh0DVio_ubpFtCVvMv3WLR1MVPQji1sN5PDNKvHCT4=")
+            )
+        )
+    )
 }
--- app/src/main/java/pt/iade/games/gamedevedex/ui/components/ProjectCard.kt
+++ app/src/main/java/pt/iade/games/gamedevedex/ui/components/ProjectCard.kt
@@ -1,40 +1,86 @@
 package pt.iade.games.gamedevedex.ui.components
 
+import android.content.Intent
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import pt.iade.games.gamedevedex.ProjectDetailActivity
+import pt.iade.games.gamedevedex.R
 import pt.iade.games.gamedevedex.models.Project
 import pt.iade.games.gamedevedex.models.Student
 import java.net.URI
 
 @Composable
 fun ProjectCard(
-    project: Project
+    project: Project,
+    modifier: Modifier = Modifier
 ) {
-    Column {
-        Text(
-            text = "${project.votes}",
-            fontSize = 17.sp
-        )
+    val context = LocalContext.current
 
-        Text("Here a background with screenshot will be placed.")
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        onClick = {
+            Toast.makeText(context, project.title,
+                Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, ProjectDetailActivity::class.java)
+            context.startActivity(intent)
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.placeholder_cover_image),
+                contentDescription = "Cover image of the game",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = "${project.votes}",
+                    fontSize = 17.sp,
+                    color = Color(255, 255, 255)
+                )
+            }
+        }
 
         Row(
+            modifier = Modifier.padding(10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
                 text = project.title,
                 fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
+                fontSize = 20.sp,
+                modifier = Modifier.weight(weight = 1f),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             Button(
                 onClick = { },
@@ -47,10 +93,11 @@ fun ProjectCard(
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview()
 fun ProjectCardPreview() {
     Column {
         ProjectCard(
+            modifier = Modifier.padding(vertical = 20.dp),
             project = Project(
                 title = "Among Us",
                 votes = 2,
@@ -73,6 +120,7 @@ fun ProjectCardPreview() {
             )
         )
         ProjectCard(
+            modifier = Modifier.padding(vertical = 20.dp),
             project = Project(
                 title = "Zelda: Twilight Princess",
                 votes = 123,
